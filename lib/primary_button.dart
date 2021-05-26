1d044718abecae4545da1b93cca4b3cf4a423e11
Baishali Roy <roybaishali07@gmail.com>
2021-05-26 17:37:58 +0530

    black signin page

---
+++ lib/primary_button.dart
@@ -0,0 +1,10 @@
+/*import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget{
+  const PrimaryButton({
+    Key key,
+    @required this.text,
+    @required this.press,
+    this.color = 
+  })
+}*/
