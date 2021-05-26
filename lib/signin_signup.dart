26425efd94826a81c6d062b990d5975e690d2933
Anirban Dutta <61903332+DuttaAB-dev@users.noreply.github.com>
2021-05-26 19:20:12 +0530

    Changed user avatar

--- lib/signin_signup.dart
+++ lib/signin_signup.dart
@@ -5,14 +5,20 @@ class SigninOrSignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            children: [
-              Image.asset(
-                'images/neon.png',
-                height: 146,
+          child: Padding(
+            padding: EdgeInsets.all(50.0),
+            child: Center(
+              child: Column(
+                children: [
+                  Image.asset(
+                    'images/user_avatar.png',
+                    height: 170,
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
