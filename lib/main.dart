7212a4d65a6f1bbaf4dff508c102712c56751dd9
Baishali Roy <roybaishali07@gmail.com>
2021-05-26 16:07:44 +0530

    coco

--- lib/main.dart
+++ lib/main.dart
@@ -8,33 +8,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            children: [
-              Image.asset("images/call.png"),
-              Text("Welcome to our Neon Messaging App",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline5),
-              Text(" \nYour privacy is our priority!",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyText1),
-              TextButton(
-                onPressed:() {},
-                child : Row(
-                  children : [
-                    Text(
-                      "<Skip>",
-                      style : Theme.of(context).textTheme.bodyText1
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+        home: Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("Welcome to our Neon Messaging App",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20)),
+            Image.asset("images/call.png"),
+            Text(" \nYour privacy is our priority!",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 16)),
+            TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Text("SKIP", style: TextStyle(color: Colors.green)),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
