551bc1a601ac4c87250fd21964a8da6bdaaabbec
Baishali Roy <roybaishali07@gmail.com>
2021-05-26 16:45:33 +0530

    coco2

---
+++ lib/signinOrSignUp/signin_signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SigninOrSignupScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Image.asset('images/neon.png')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
