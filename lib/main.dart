395e929adb56ca5c45576cf8149a9157323cd3af
Baishali Roy <roybaishali07@gmail.com>
2021-05-26 14:52:43 +0530

    Added required files

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Image.asset("images/call.png"),
+              Text("Welcome to our Neon Messaging App",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline5),
+              Text(" \nYour privacy is our priority!",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyText1),
+              TextButton(
+                onPressed:() {},
+                child : Row(
+                  children : [
+                    Text(
+                      "<Skip>",
+                      style : Theme.of(context).textTheme.bodyText1
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
