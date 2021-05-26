bbb634f839dc4a4a375eb9ae09425b51fe41c795
Baishali Roy <roybaishali07@gmail.com>
2021-05-26 15:31:11 +0530

    enter

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: [
